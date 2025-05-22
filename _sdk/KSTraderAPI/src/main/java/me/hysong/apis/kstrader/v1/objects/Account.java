6552942e2e69634005574e9266cf6889ba05915d
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2025-05-22 22:48:57 +0900

    Account removed unnecessary fields

--- _sdk/KSTraderAPI/src/main/java/me/hysong/apis/kstrader/v1/objects/Account.java
+++ _sdk/KSTraderAPI/src/main/java/me/hysong/apis/kstrader/v1/objects/Account.java
@@ -12,8 +12,6 @@ public class Account {
     public static final String CREDENTIAL_KEY_PK = "pk";
     public static final String CREDENTIAL_KEY_SK = "sk";
 
-    private final String accountId;
-    private final String accountName;
     private final String accountType; // e.g. "spot", "futures"
     private final String exchange; // e.g. "binance", "upbit"
 
@@ -21,17 +19,13 @@ public class Account {
     private HashMap<String, Chart> charts; // e.g. {"Spot:BTCUSDT": ChartObject}
     private HashMap<String, ArrayList<Order>> orders; // e.g. {"Spot:BTCUSDT": [OrderObject1, OrderObject2]}
 
-    public Account(String accountId, String accountName, String accountType, String exchange, HashMap<String, Object> credentials) {
-        this.accountId = accountId;
-        this.accountName = accountName;
+    public Account(String accountType, String exchange, HashMap<String, Object> credentials) {
         this.accountType = accountType;
         this.exchange = exchange;
         this.credentials = credentials;
     }
 
     public Account(String accountType, String exchange, String pk, String sk) {
-        this.accountId = SIDKit.generateSID(SIDKit.SIDType.USER_OBJECT);
-        this.accountName = "Default Account";
         this.accountType = accountType;
         this.exchange = exchange;
         this.credentials = new HashMap<>();
