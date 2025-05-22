7161b5f8c8200ea3fcb13c0f5c8dccf8cebbedf7
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2025-05-22 22:49:07 +0900

    Update GeneralV1OverWSV1.java

--- _dStrategies/GeneralV1OverWS/src/main/java/me/hysong/kstrader/strategies/generalv1overws/GeneralV1OverWSV1.java
+++ _dStrategies/GeneralV1OverWS/src/main/java/me/hysong/kstrader/strategies/generalv1overws/GeneralV1OverWSV1.java
@@ -1,6 +1,13 @@
 package me.hysong.kstrader.strategies.generalv1overws;
 
+import me.hysong.apis.kstrader.v1.driver.TraderDriverManifestV1;
+import me.hysong.apis.kstrader.v1.driver.TraderDriverV1;
+import me.hysong.apis.kstrader.v1.objects.Account;
 import me.hysong.apis.kstrader.v1.strategy.WSStrategyV1;
 
 public class GeneralV1OverWSV1 implements WSStrategyV1 {
+    @Override
+    public void loop(Account account, String[] symbols, TraderDriverManifestV1 driverManifest, TraderDriverV1 driver) throws Exception {
+
+    }
 }
