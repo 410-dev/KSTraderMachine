cdc3154f8e8a1c86a352fcbc890daae6be41e48e
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2025-05-29 00:07:44 +0900

    Update StorageSetupTool.java

--- src/main/java/me/hysong/kynesystems/apps/kstradermachine/backend/startup/StorageSetupTool.java
+++ src/main/java/me/hysong/kynesystems/apps/kstradermachine/backend/startup/StorageSetupTool.java
@@ -6,6 +6,7 @@ import me.hysong.atlas.async.SimplePromise;
 import me.hysong.atlas.sdk.graphite.v1.GPSplashWindow;
 import me.hysong.atlas.utils.MFS1;
 import me.hysong.atlas.utils.VFS2;
+import me.hysong.atlas.utils.vfs.v3.VFS3;
 import me.hysong.kynesystems.apps.kstradermachine.Application;
 
 import javax.swing.*;
@@ -14,12 +15,15 @@ import java.io.File;
 public class StorageSetupTool {
 
     public static void copyDefault(GPSplashWindow splashWindow, String storagePath) {
-        // TODO: Implement the copy default function.
-
         // Recursively list files in default location
-        String defaultVFSLocation = "defaults.vfs2";
-        VFS2 vfs = new VFS2(defaultVFSLocation);
+        VFS3 vfs = new VFS3("struct.img.vfs3");
         vfs.imageToRealDisk(new File(storagePath));
+
+        // Set storage initialized to true
+        JsonObject storageDeclaration = new JsonObject();
+        storageDeclaration.addProperty("version", 1);
+        storageDeclaration.addProperty("initialized", true);
+        MFS1.write(storagePath + "/storage.json", storageDeclaration.toString());
     }
 
     public static String init(GPSplashWindow splashWindow) {
